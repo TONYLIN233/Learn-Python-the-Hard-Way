3087dc176b95d8edd8206de8eed0ae7df42483d4
Administrator <274276305@qq.com>
2025-09-10 02:04:36 +0800

    feat：练习3

---
+++ 3.py
@@ -0,0 +1,23 @@
+print (  "I will now count my chickens:")
+
+print   ("Hens",   25   +   30   /   6)
+
+print("Roosters", 100 - 25 * 3 % 4)
+print("Now I will count the eggs:")
+
+print(3+2+1-5+4%2+6)
+print("Is it true that 3+2<5-7?")
+print(3+2<5-7)
+
+print("what is 3+2?",3+2)
+print("What is 5-7?",5-7)
+print("Oh, that's why it's false.")
+print("How about some more.")
+print("Is it greedy.",5 > -2)
+print("Is it greedy.",5 >= -2)
+print("Is it greedy.",5 <= -2)
+
+
+print("is float",4.0 / 3.0)
+#python3中修复了python2.X中整数 / 整数的结果会自动向下取整（地板除）的问题
+print("is int",4 / 3)
